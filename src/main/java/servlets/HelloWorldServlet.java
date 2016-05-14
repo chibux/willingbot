33809ed2f192bb969eb1c6e6b482a04daf9ae322
trierra <trierra.dev@gmail.com>
2016-05-13 21:00:11 -0700

    clean

--- src/main/java/servlets/HelloWorldServlet.java
+++ src/main/java/servlets/HelloWorldServlet.java
@@ -1,15 +1,22 @@
 package servlets;
 
+import org.apache.http.HttpHost;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicHeader;
 import org.json.JSONObject;
 
-import javax.net.ssl.HttpsURLConnection;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.*;
-import java.net.URL;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URI;
 import java.util.Map;
 
 /**
@@ -20,20 +27,18 @@ import java.util.Map;
 public class HelloWorldServlet extends HttpServlet {
     private static String PAGE_TOKEN = "EAAIbsSXN1U8BAM39eXNXAI8Hj5rUwaTG1zOw6Pfdiwfkc2QnAMD4ZBXJOkakDwaHbV5kuouSKZCPAIcZB3xDZBUaZCuuXXWuSt2vuMQRz6y2QDslJQZBI5ZC4ZBz1u8q4RaFzvX8PZCExsicgzvUd19K1cMwLNQgZC6ZAZBWGBFInXt1NQZDZD";
 
-    private final String USER_AGENT = "Mozilla/5.0";
-
 
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         Map<String, String[]> requestMap = request.getParameterMap();
-        if (!requestMap.isEmpty()){
-            if(requestMap.get("hub.verify_token")[0].equals("mytoken")){
+        if (!requestMap.isEmpty()) {
+            if (requestMap.get("hub.verify_token")[0].equals("mytoken")) {
                 PrintWriter writer = response.getWriter();
                 writer.println(requestMap.get("hub.challenge")[0]);
             }
-        }else {
+        } else {
             PrintWriter writer = response.getWriter();
             writer.println("error");
         }
@@ -53,60 +58,30 @@ public class HelloWorldServlet extends HttpServlet {
         System.out.println("time " + time);
 
         JSONObject senderId = messaging.getJSONObject("sender");
-//        System.out.println("senderId " + senderId);
-//        String recipientId = messaging.getJSONObject("recipient").get("id").toString();
-//        System.out.println("recipientId " + recipientId);
-//
-////        String message = messaging.getJSONObject("message").getString("text");
-////        System.out.println("message = " + message);
-//        Long timestamp = messaging.getLong("timestamp");
-//        System.out.println("timestamp " + timestamp);
-
-        sendMessage(senderId);
+        if (!messaging.isNull("message")) {
+            sendMessage(senderId);
+        }
 
     }
+
     private void sendPost(String message) throws Exception {
 
-        String url = "https://graph.facebook.com/v2.6/me/messages?";
+        String urlParameters = "access_token=" + PAGE_TOKEN;
 
-        //add request header
-        //con.setRequestProperty("User-Agent", USER_AGENT);
-        //con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
-
-        String urlParameters = "access_token="+ PAGE_TOKEN;
-        url = url.concat(urlParameters);
-        URL obj = new URL(url);
-        HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
-        con.setRequestMethod("POST");
-        // Send post request
-        con.setDoOutput(true);
-        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-        wr.write(message.getBytes("UTF-8"));
-        wr.flush();
-        wr.close();
-
-        int responseCode = con.getResponseCode();
-        System.out.println("\nSending 'POST' request to URL : " + url);
-        System.out.println("Post parameters : " + urlParameters);
-        System.out.println("Response Code : " + responseCode);
-
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer response = new StringBuffer();
-
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpHost https = new HttpHost("graph.facebook.com", 443, "https");
+            HttpPost request = new HttpPost();
+            request.setURI(new URI("/v2.6/me/messages?" + urlParameters));
+            request.setHeader(new BasicHeader("Content-Type", "application/json"));
+            request.setEntity(new StringEntity(message));
+            CloseableHttpResponse execute = httpClient.execute(https, request);
+            System.out.print(execute.getStatusLine().getStatusCode());
+            System.out.print(new java.util.Scanner(execute.getEntity().getContent(), "UTF-8").useDelimiter("\\A"));
         }
-        in.close();
-
-        //print result
-        System.out.println(response.toString());
-
     }
 
 
-    private void sendMessage(JSONObject senderId){
+    private void sendMessage(JSONObject senderId) {
 
         JSONObject recipient = new JSONObject();
         recipient.put("recipient", senderId);
@@ -122,28 +97,6 @@ public class HelloWorldServlet extends HttpServlet {
             e.printStackTrace();
         }
 
-
-//        ClientConfig config = new DefaultClientConfig();
-//        SSLContext ctx = null;
-//        try {
-//            ctx = SSLContext.getInstance("SSL");
-//        } catch (NoSuchAlgorithmException e) {
-//            e.printStackTrace();
-//        }
-//        ctx.init(null, myTrustManager, null);
-//        config.getProperties().put(HTTPSProperties.PROPERTY_HTTPS_PROPERTIES, new HTTPSProperties(hostnameVerifier, ctx));
-//        Client client = Client.create(config);
-//
-//
-//
-//
-////        WebResource resource = client.resource("https://graph.facebook.com/v2.6/me/messages").queryParam("access_token", PAGE_TOKEN);
-
-//        ClientResponse response = service.accept("application/json").post(ClientResponse.class, recipient.toString());
-
-
-
-//        System.out.println(response.getStatus());
     }
 
 
--- src/main/java/servlets/JsonTest.java
+++ src/main/java/servlets/JsonTest.java
@@ -2,36 +2,39 @@ package servlets;
 
 import org.json.JSONObject;
 
+import java.util.Set;
+
 /**
  * @author trierra
  * @date 5/12/16.
  */
 public class JsonTest {
     public static void main(String[] args) {
-        String messageReq = "{\"object\":\"page\",\"entry\":" +
-                "[{\"id\":1562003140766937,\"time\":1463089116181,\"messaging\":" +
-                "[{\"sender\":{\"id\":1059287277465794},\"recipient\":" +
-                "{\"id\":1562003140766937},\"timestamp\":1463089115964,\"message\":" +
-                "{\"mid\":\"mid.1463089115860:924f8507e8f4a16858\",\"seq\":24,\"text\":\"hey\"}}]}]}";
+        String messageReq = "{entry: {\"id\":1562003140766937,\"time\":1463196328281,\"messaging\":[{\"sender\":{\"id\":1059287277465794},\"recipient\":{\"id\":1562003140766937},\"message\":{\"attachments\":[{\"payload\":{\"coordinates\":{\"lat\":37.323133654289,\"long\":-121.98682162007}},\"title\":\"Oksana's Location\",\"type\":\"location\",\"url\":\"https://www.facebook.com/l.php?u=https%3A%2F%2Fwww.bing.com%2Fmaps%2Fdefault.aspx%3Fv%3D2%26pc%3DFACEBK%26mid%3D8100%26where1%3D37.323133654289%252C%2B-121.98682162007%26FORM%3DFBKPL1%26mkt%3Den-US&h=QAQGRtbZL&s=1&enc=AZPaqnL11Zms30vr8gy0argiiIx0CRSWeFYal2JuiFloPwee6BG_RxrOim7G14aaIg48FXtEiUMSZmFoawwBdD0TinA64YDHcko17_F3CIO_tw\"}]," +
+                "\"mid\":\"mid.1463196327401:01068a02daa9c98654\",\"seq\":267},\"timestamp\":1463196328122}]}}\n";
 
 
         JSONObject jsonObject = new JSONObject(messageReq);
-        JSONObject entry = jsonObject.getJSONArray("entry").getJSONObject(0);
-        System.out.println(entry);
+        JSONObject entry = jsonObject.getJSONObject("entry");
 
         JSONObject messaging = entry.getJSONArray("messaging").getJSONObject(0);
         Long time = entry.getLong("time");
-        System.out.println("time " + time);
 
         String senderId = messaging.getJSONObject("sender").get("id").toString();
-        System.out.println("senderId " + senderId);
         String recipientId = messaging.getJSONObject("recipient").get("id").toString();
-        System.out.println("recipientId " + recipientId);
+        Set keyset = messaging.keySet();
+
+        if (keyset.contains("message")){
+            JSONObject message = messaging.getJSONObject("message");
+            if(message.keySet().contains("text")){
+                System.out.println(message.getString("text"));
+            }
+        }
 
         String message = messaging.getJSONObject("message").getString("text");
-        System.out.println("message = " + message);
         Long timestamp = messaging.getLong("timestamp");
-        System.out.println("timestamp " + timestamp);
+        JSONObject attachment = messaging.getJSONObject("attachment");
+        System.out.println(attachment);
 
     }
 }

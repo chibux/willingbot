168f0452676b7ef29342280e06daf588e0c32987
trierra <trierra.dev@gmail.com>
2016-05-13 23:47:56 -0700

    added json utils

--- src/main/java/servlets/JsonUtils.java
+++ src/main/java/servlets/JsonUtils.java
@@ -1,6 +1,7 @@
 package servlets;
 
 import org.json.JSONObject;
+import sun.nio.cs.ext.SJIS;
 
 import java.util.Set;
 
@@ -8,7 +9,8 @@ import java.util.Set;
  * @author trierra
  * @date 5/12/16.
  */
-public class JsonTest {
+public class JsonUtils {
+
     public static void main(String[] args) {
         String messageReq = "{entry: {\"id\":1562003140766937,\"time\":1463196328281,\"messaging\":[{\"sender\":{\"id\":1059287277465794},\"recipient\":{\"id\":1562003140766937},\"message\":{\"attachments\":[{\"payload\":{\"coordinates\":{\"lat\":37.323133654289,\"long\":-121.98682162007}},\"title\":\"Oksana's Location\",\"type\":\"location\",\"url\":\"https://www.facebook.com/l.php?u=https%3A%2F%2Fwww.bing.com%2Fmaps%2Fdefault.aspx%3Fv%3D2%26pc%3DFACEBK%26mid%3D8100%26where1%3D37.323133654289%252C%2B-121.98682162007%26FORM%3DFBKPL1%26mkt%3Den-US&h=QAQGRtbZL&s=1&enc=AZPaqnL11Zms30vr8gy0argiiIx0CRSWeFYal2JuiFloPwee6BG_RxrOim7G14aaIg48FXtEiUMSZmFoawwBdD0TinA64YDHcko17_F3CIO_tw\"}]," +
                 "\"mid\":\"mid.1463196327401:01068a02daa9c98654\",\"seq\":267},\"timestamp\":1463196328122}]}}\n";
@@ -31,10 +33,37 @@ public class JsonTest {
             }
         }
 
-        String message = messaging.getJSONObject("message").getString("text");
         Long timestamp = messaging.getLong("timestamp");
-        JSONObject attachment = messaging.getJSONObject("attachment");
+        JSONObject attachment = messaging.getJSONObject("message").getJSONArray("attachments").getJSONObject(0);
         System.out.println(attachment);
+
+    }
+
+    public static JSONObject getMessagingObject(JSONObject entry){
+
+    }
+
+    public static JSONObject getSenderObject(JSONObject entry){
+
+    }
+
+    public static JSONObject getMessageObject(JSONObject entry){
+
+    }
+
+    public static JSONObject getCoordinatesObject(JSONObject entry){
+
+    }
+
+    public static  JSONObject getPayloadObject(JSONObject entry){
+
+    }
+
+    public static JSONObject getLocationTitleObject(JSONObject entry){
+
+    }
+
+    public static JSONObject getLoocationUrlObject(JSONObject entry){
 
     }
 }
